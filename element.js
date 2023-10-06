15ccf74e8072c4b6bae58b7c03d90d9b5d6115ca
THAIS DE BIAZZI COSTA <thais.bcosta4@senacsp.edu.br>
2023-10-06 17:17:48 -0300

    aula

---
+++ element.js
@@ -0,0 +1,9 @@
+//CRIANDO UMA VARIAVEL PARAGRAFO E CRIANDO A TAG P
+
+let paragrafo = document.createElement("p")
+
+//COLOCANDO O TEXTO NO PARAGRAFO
+paragrafo.innerText = "Isso aqui é um paragrafo"
+
+//INCORPORANDO UM PARAGRAFO NO BODY HTML
+document.body.appendChild(paragrafo)
